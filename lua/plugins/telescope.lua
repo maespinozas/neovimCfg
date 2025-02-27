ecea9d4ec1f0206c98559de722269d74a125b9cc
Marco_Espinoza (uidp1615) <marco.antonio.espinoza.sanchez@continental-corporation.com>
2025-02-27 16:15:59 -0600

    added shorcut to ripgrep word under cursor with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,10 +11,13 @@ return {
       config = function()
 
       local builtin = require('telescope.builtin')
+      local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>gw', builtin.grep_string, { desc = 'Grep word under cursor' })
+      vim.keymap.set("n", "<leader>fw", live_grep_args_shortcuts.grep_word_under_cursor, { desc = 'Grep word under cursor with args'})
 
       vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
