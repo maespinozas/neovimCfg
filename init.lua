6b274cc22141d0c1537a88a94b641b8a5ee519a9
Marco_Espinoza (uidp1615) <marco.antonio.espinoza.sanchez@continental-corporation.com>
2025-09-24 09:34:11 -0600

    Added git fugitive plugin, updated init.lua file to set shiftwidth

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 require('config.lazy')
 
 require('user.keymaps')
+require('user.exampleModule')
 
 -- Custom vim config --
 vim.opt.swapfile = false
 
 --set number
 vim.wo.number = true
+
+--set shiftwidth
+vim.opt.shiftwidth = 2
