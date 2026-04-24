2b2d88871b7fd276ba9b9b94e84b4d59623eae31
Marco_Espinoza (uidp1615) <marco.antonio.espinoza.sanchez@continental-corporation.com>
2026-04-24 15:18:33 -0600

    prepared nvim-treesitter to use main branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,59 +1,40 @@
 
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master', -- This is the critical line to keep the old version
-    lazy = false,      -- This plugin does not support lazy-loading [citation:5]
-    build = function()
-    	--set this line for Windows to use clang to compile parsers
-    	--require('nvim-treesitter.install').compilers = { "clang", "gcc" }
-        require("nvim-treesitter.install").update({ with_sync = true })()
+  "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        -- Install parsers (replaces ensure_installed from old API)
+        ts.install({
+            "lua",
+            "python",
+            "c",
+            "cpp",
+            "markdown",
+            "html",
+            "javascript",
+            "json",
+            "bash",
+            "vim",
+            "vimdoc",
+        })
+
+        -- Optional: Disable highlighting for specific filetypes if needed
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "large_file", "some_language" },
+            callback = function(args)
+                vim.treesitter.stop(args.buf)
+            end,
+        })
+
+        -- Configure folding (same as before)
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.opt.foldlevel = 99
     end,
-    config = function ()
-	    require("nvim-treesitter.configs").setup({
-		--List of langauges to install by default
-		ensure_installed ={
-			"lua",
-			"python",
-			"c",
-			"cpp",
-			"markdown",
-			"html",
-			"javascript",
-			"json",
-			"bash",
-			"vim",
-			"vimdoc",
-		},
-	    sync_install = true,
-
-	    --Enable syntax highligthing
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            -- Enable folding
-            folds = {
-                enable = true,
-            },
-            -- Optional: Enable incremental selection
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    node_incremental = "v",
-                    node_decremental = "V",
-                },
-            },
-
-	    })
-	    -- Folding
-	    vim.opt.foldmethod = "expr"
-	    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-	    -- Optional : start with all folds open
-	    vim.opt.foldlevel = 99
-
-
-    end
 }
 
 return { M }
