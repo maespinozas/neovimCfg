48adff611f711aa822f511a112dfb7779cf09acc
Marco Espinoza <maespinozas0188@gmail.com>
2026-04-23 20:08:50 -0600

    Updated config for Mason to be compatible with neovim .11 Updated so lsp is configured with native vim function instead of mason handlers

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -79,67 +79,33 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["pyright"] = function()
-        -- configure pyright
-        lspconfig["pyright"].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["kotlin_language_server"] = function()
-        -- configure kotlin
-        lspconfig["kotlin_language_server"].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["clangd"] = function()
-        -- configure clangd
-        lspconfig["clangd"].setup({
-          capabilities = capabilities,
-        })
-      end,
-      --[[
-      ["svelte"] = function()
-        -- configure svelte server
-        lspconfig["svelte"].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
-              callback = function(ctx)
-                -- Here use ctx.match instead of ctx.file
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-              end,
-            })
-          end,
-        })
-      end,
-      ["graphql"] = function()
-        -- configure graphql language server
-        lspconfig["graphql"].setup({
-          capabilities = capabilities,
-          filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-        })
-      end,
-      ["emmet_ls"] = function()
-        -- configure emmet language server
-        lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-        })
-      end,
-      --]]
+    -- Server-specific settings for pyright
+    vim.lsp.config("pyright", {
+      settings = {
+	python = {
+	  analysis = {
+	    typeCheckingMode = "basic",
+	    autoSearchPaths = true,
+	    useLibraryCodeForTypes = true,
+	  },
+	},
+      },
+    })
 
-      ["lua_ls"] = function()
-        -- configure lua server (with special settings)
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
+    -- Server-specific settings for kotlin_language_server
+    vim.lsp.config("kotlin_language_server", {
+      -- Add kotlin-specific settings here
+      settings = {},
+    })
+
+    -- Server-specific settings for clangd
+    vim.lsp.config("clangd", {
+      cmd = { "clangd", "--background-index" },
+      capabilities = capabilities,
+    })
+    -- Server-specific settings for lua
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
           settings = {
             Lua = {
               -- make the language server recognize "vim" global
@@ -151,8 +117,8 @@ return {
               },
             },
           },
-        })
-      end,
     })
+
+
   end,
 }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,6 +4,51 @@ local M = {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function ()
+	    require("nvim-treesitter.configs").setup({
+		--List of langauges to install by default
+		ensure_installed ={
+			"lua",
+			"python",
+			"c",
+			"cpp",
+			"markdown",
+			"html",
+			"javascript",
+			"json",
+			"bash",
+			"vim",
+			"vimdoc",
+		},
+
+	    --Enable syntax highligthing
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            -- Enable folding
+            folds = {
+                enable = true,
+            },
+            -- Optional: Enable incremental selection
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                },
+            },
+
+	    })
+	    -- Folding
+	    vim.opt.foldmethod = "expr"
+	    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+	    -- Optional : start with all folds open
+	    vim.opt.foldlevel = 99
+
+
+    end
 }
 
 return { M }
