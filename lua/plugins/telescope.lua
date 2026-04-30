a9e6d5bbb64d40cef4b6f362198ecdf4ca07cc01
Marco_Espinoza (uidp1615) <marco.antonio.espinoza.sanchez@continental-corporation.com>
2026-04-30 08:54:45 -0600

    Updated telescope to track master branch Subject line (50 chars max)
    Body of the commit message (72 chars max per line)
    
    - Bullet point 1
    - Bullet point 2

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 
 -- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim', 
+    ---tag = '0.1.8',
 -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim',
 	"nvim-telescope/telescope-live-grep-args.nvim", 
