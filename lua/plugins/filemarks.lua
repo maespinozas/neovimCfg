ef6ce7a4577ba6c5617a41c4caf4428e82ffb911
Marco_Espinoza (uidp1615) <marco.antonio.espinoza.sanchez@continental-corporation.com>
2026-05-18 16:27:44 -0600

    added storage path to json file Subject line (50 chars max)
    Body of the commit message (72 chars max per line)
    
    - Bullet point 1
    - Bullet point 2

--- lua/plugins/filemarks.lua
+++ lua/plugins/filemarks.lua
@@ -5,6 +5,7 @@ return
         require('filemarks').setup({
             goto_prefix = "<leader>m",  -- This is the prefix for jumping to marks
             action_prefix = "<leader>M", -- This is for adding marks
+	    storage_path = vim.fn.expand("~") .. "/filemarks.json",
 	  dir_open_cmd = function(path)
 	    require("nvim-tree.api").tree.open({ path = path })
 	  end,
